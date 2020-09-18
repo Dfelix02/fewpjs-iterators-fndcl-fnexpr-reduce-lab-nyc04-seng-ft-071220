e27247d46f0b5dd2ec98618266aa6cde88f644a2
David Felix <david.felix02@hotmail.com>
2020-09-18 03:45:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-let totalBatteries = batteryBatches.reduce((total,element)=>{return element+total}, 0);
+let totalBatteries = batteryBatches.reduce((total,element)=>{
+  console.log(total);
+  return element+total
+  
+}, 0);
